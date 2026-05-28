3b63f2dadcaa3739c376088174d837c9e4eddef1
Sami <Samiayalew26@gmail.com>
2026-05-28 20:38:00 +0300

    sending tokens by using cookies

--- controlers/authController.js
+++ controlers/authController.js
@@ -12,6 +12,15 @@ const signToken = (id) => {
 };
 const createSendToken = (user, statuscode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined;
   res.status(statuscode).json({
     status: 'success',
     token,
@@ -55,7 +64,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
-    console.log(token);
+    //console.log(token);
   }
   if (!token) {
     return next(
